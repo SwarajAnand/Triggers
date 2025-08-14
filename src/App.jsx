780aec6fbbd09bd566dddace3eb2aaa02a351066
swaraj anand <swarajanand@swarajs-MacBook-Air.local>
2025-08-14 23:09:53 +0530

    test 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const App = () => {
+  const [count, setCount] = React.useState(0);
+
+  for(let i = 0; i < 1000000; i++) {
+    // Simulate some work
+  }
   return (
-    <div>Testing the app</div>
+    <div>
+      <p>Testing the app</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <p>Count: {count}</p>
+    </div>
   )
 }
 
