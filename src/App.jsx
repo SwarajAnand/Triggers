81fe5dcf4fa56b089bcbae03d9787f3fa5653596
swaraj anand <swarajanand@swarajs-MacBook-Air.local>
2025-08-14 21:36:36 +0530

    Test branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 
 const App = () => {
+  const [result, setResult] = React.useState(null);
+
+  function handleAdd() {
+    const sum = add(2, 3);
+    setResult(sum);
+  }
+
   function add(a, b) {
     return a + b;
   }
-  return <div>Testing the app</div>;
+  return (
+    <div>
+      Testing the app
+      <button onClick={handleAdd}>Add</button>
+      {result !== null && <p>Result: {result}</p>}
+    </div>
+  );
 };
 
 export default App;
