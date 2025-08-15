555a0a42c3747a74a47c6a0d7e515e668f2aa413
swaraj anand <swarajanand@swarajs-MacBook-Air.local>
2025-08-16 03:29:53 +0530

    Test 05

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const App = () => {
+  const [count, setCount] = React.useState(0);
+  for (let i = 0; i < 1000000000; i++) {};
+  
   return (
     <div>Testing the app</div>
   )
